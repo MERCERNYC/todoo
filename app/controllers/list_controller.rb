6753f8c37702aa8eb611b41a2738eb115a3e0181
thelucasmoore <lucasedwardmoore@gmail.com>
2016-03-24 16:44:18 -0500

    task new/edit/delete working

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -5,4 +5,35 @@ class ListsController < ApplicationController
     erb :'/lists/new'
   end
 
+  post '/lists/new' do
+    list = List.create(:name => params[:name], :user_id => session[:user_id])
+    task = Task.create(:name => params[:task])
+    list.tasks << task unless list.tasks.include?(task)
+    redirect '/tasks'
+  end
+
+  get '/lists/:id/edit' do
+    redirect_if_not_logged_in
+    @list = List.find_by_id(params[:id])
+    erb :'lists/edit'
+  end
+
+  patch '/lists/:id' do 
+    @list = List.find_by_id(params[:id])
+    @list.name = params[:name]
+    @list.save
+    redirect '/tasks'
+  end
+
+  get '/lists/:id/delete' do
+    @list = List.find_by_id(params[:id])
+    erb :'lists/delete'
+  end
+
+  delete '/lists/:id' do 
+    @list = List.find_by_id(params[:id])
+    @list.delete
+    redirect '/tasks'
+  end
+
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,11 +3,17 @@ class TasksController < ApplicationController
   get '/tasks' do
     redirect_if_not_logged_in
     @user = current_user
+    @lists = List.all
+    #@tasks = List.tasks
     erb :'tasks/show'
   end
 
   get '/tasks/new' do
     redirect_if_not_logged_in
     erb :'/tasks/new'
+  end
+
+  get '/tasks/:id' do
+    "Do a thing!"
   end
 end
